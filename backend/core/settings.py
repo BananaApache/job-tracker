18c86c07842fc9d2ef182aa7bca2bbc89a87ea8f
bananaapache <110048127+BananaApache@users.noreply.github.com>
2026-02-28 19:16:25 -0500

    feat: add deploy.sh

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,7 +31,7 @@ else:
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
